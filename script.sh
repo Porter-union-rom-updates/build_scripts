081642cb1ef22575db2101a5637759540b1f7ebe
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-06 14:37:49 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b pixelstar .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_co -b pixelstar/14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
