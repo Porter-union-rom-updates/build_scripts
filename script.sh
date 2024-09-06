65113d45b084f1b0f11e3324e61110aa216e8cdf
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-06 14:41:23 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,23 +20,6 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# Use AOSP clang for no errors thanks trijal kido.
-rm -r prebuilts/clang/host/linux-x86
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 prebuilts/clang/host/linux-x86
-echo "===== fix  AOSP clang for no errors ====="
-
-# Git cherry-pick
-cd vendor/lineage
-git remote add los https://github.com/LineageOS/android_vendor_lineage
-git fetch los
-git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
-cd ../..
-echo "===== Cherry-picking Success ====="
-
-# Private keys
-git clone https://github.com/Gtajisan/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
-echo "===== cp clone done ====="
-
 # Export
 export BUILD_USERNAME=FARHAN_UN
 export BUILD_HOSTNAME=crave
@@ -47,7 +30,7 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch pixelstar_Mi439_4_19-ap2a-userdebug
+lunch pixelstar_mi439-ap2a-userdebug
 echo "====== lunch Done ======="
 
 # installclean
