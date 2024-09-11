50a205fe4bd760570a46ee304a2f75457570ad1d
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-11 06:48:34 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,7 +22,8 @@ echo "============="
 
 # Export
 export BUILD_USERNAME=FARHAN_UN
-export BUILD_HOSTNAME=crave
+export BUILD_HOSTNAME=crave 
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 echo "======= Export Done ======"
 
 # Set up build environment
