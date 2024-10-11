db6016e8f4493020990c875fc475df4c8e6ccec0
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-11 12:38:43 +0600

    Ok

--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Remove existing local_manifests
 rm -rf .repo/local_manifests/
 
-# repo init rom
-repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs
+# Initialize git lfs
+git lfs install
+
+# repo init manifest
+repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -15,21 +19,38 @@ echo "Local manifest clone success"
 echo "============================"
 
 # Sync
-/opt/crave/resync.sh 
+/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 echo "============="
 echo "Sync success"
 echo "============="
 
+# Private keys
+git clone https://github.com/Trijal08/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME=FARHAN_UN
-export BUILD_HOSTNAME=crave
+export BUILD_USERNAME="FARHAN"
+export BUILD_HOSTNAME="crave"
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
 
 # Set up build environment
-. build/envsetup.sh 
+source build/envsetup.sh
 echo "====== Envsetup Done ======="
+
+# Lunch
+breakfast mi439
+make installclean -j$(nproc --all)
+echo "============="
+
+# Build ROM
+croot
+brunch mi439
 
 # Lunch
 lunch orion_mi439-ap2a-userdebug || lunch orion_mi439-ap3a-userdebug || lunch orion_mi439_userdebug
 mka space
+
+
+
+
+
