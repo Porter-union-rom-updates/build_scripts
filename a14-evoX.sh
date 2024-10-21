a9c82077d9efcf048d9600d7f6fe99de2b75776c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-21 11:13:58 +0600

    Your base

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -3,40 +3,41 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b evoX-14 .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b a14-crd .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# build
-/opt/crave/resync.sh 
+# Sync
+/opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-cd packages/apps/Dialer
-git fetch github --unshallow
-git fetch https://github.com/ProjectBlaze/packages_apps_Dialer.git 14
-git cherry-pick f0052a5
-cd ../../..
+# keys
+# mkdir vendor/lineage-priv
+# cp build-keys/* vendor/lineage-priv
+# echo "============="
+# echo "Keys copied"
+# echo "============="
 
 # Export
-export BUILD_USERNAME=FARHAN
+export BUILD_USERNAME=FARHAN 
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
 # Set up build environment
-source build/envsetup.sh 
+source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lunch lineage_Mi439-user || lunch lineage_Mi439_4_19-user
+lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap3a-userdebug || lunch lineage_Mi439_4_19-ap2a-userdebug
 echo "============="
 
 # Make cleaninstall
