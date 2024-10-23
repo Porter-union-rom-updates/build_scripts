eb6d1eb60510123b96415d1ab6bf51519336a23c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-23 23:18:29 +0600

    🗿

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b Pixel-os .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b Pixel/14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -20,24 +20,8 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# cherry pick 1
-https://github.com/BlissRoms/platform_manifest/blob/universe/bliss.xml#L66
-cd hardware/interfaces
-git remote add bliss https://github.com/BlissRoms/platform_hardware_interfaces
-git fetch bliss
-git cherry-pick bbb2b5846645749fe3b778c1e50ac55378fd6235
-cd ../..
-
-# check pick 2
-https://github.com/BlissRoms/platform_manifest/blob/universe/bliss.xml#L73C18-L73C32
-cd kernel/configs
-git remote add bliss1 https://github.com/BlissRoms/platform_kernel_configs
-git fetch bliss1
-git cherry-pick 5da1af4336121276bd65437e329c95c770923a35
-cd ../..
-
 # Export
-export BUILD_USERNAME=FARHAN_UN
+export BUILD_USERNAME=FARHAN
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
@@ -46,7 +30,8 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch aosp_mi439-ap2a-userdebug  
+lunch aosp_mi439-ap2a-userdebug 
+lunch aosp_mi439-userdebug  
 make installclean
 mka bacon
 
