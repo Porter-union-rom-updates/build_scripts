5ab30e186e8cc613ea74afe3a51d5b1d3531852a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-26 18:26:27 +0600

    Added

--- script.sh
+++ script.sh
@@ -1,11 +1,55 @@
+#!/bin/bash
+
 rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
-#Local manifests
-git clone https://github.com/Gtajisan/local_manifests .repo/local_manifests -b a14
-#build
+
+repo init -u https://github.com/Gtajisan/android.git -b 14.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14-crd .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
 /opt/crave/resync.sh
-. build/envsetup.sh
-lunch lineage_Mi439-ap1a-userdebug
+echo "============="
+echo "Sync success"
+echo "============="
+
+#Framework_base patches
+cd frameworks/base
+git fetch crdroid --unshallow
+git fetch https://github.com/RisingTechOSS/android_frameworks_base fourteen
+#Force LTA CA
+git cherry-pick 9756c4c
+#Downgrade
+git cherry-pick ba93896
+cd ../..
+
+#toggle for LTE CA
+cd packages/apps/Settings
+git fetch crdroid --unshallow
+git fetch https://github.com/RisingTechOSS/android_packages_apps_Settings fourteen
+git cherry-pick 041f5f0
+cd ../../..
+
+#json support for PIF
+cd frameworks/base
+git fetch https://github.com/PhantomEnigma/android_frameworks_base 14.0
+git cherry-pick b8c1f87
+git cherry-pick 5d03127
+cd ../..
+
+# Export
+#export BUILD_USERNAME=Phantom
+#echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+lunch lineage_Mi439_4_19-ap2a-userdebug || lunch lineage_Mi439_4_19-userdebug
 make installclean
-brunch Mi439_4_19 || brunch lineage_Mi439_4_19-ap1a-userdebug
+mka bacon
