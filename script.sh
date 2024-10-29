b5507e327130d8fe537ef0a0c51e0b2e5a7f8608
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-29 22:10:10 +0600

    Mango

--- script.sh
+++ script.sh
@@ -2,43 +2,49 @@
 
 rm -rf .repo/local_manifests/
 
-
 # repo init rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
-echo "=============================="
-echo "gay DerpFest Repo init success"
-echo "=============================="
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b 14/derp .repo/local_manifests
-echo "========================================="
-echo "Fuckin# shit Local manifest clone success"
-echo "========================================="
+git clone https://github.com/Gtajisan/local_manifests -b a14-derp .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # build
 /opt/crave/resync.sh
-echo "======================"
-echo "Sync successfully Done"
-echo "======================"
+echo "============="
+echo "Sync success"
+echo "============="
+
+
+# Remove overrides
+# Define a list of packages to remove
+echo "===== Remove overrides started ====="
+OVER_PACKAGES=("Dialer" "Contacts Contacts2" "messaging" "DeskClock" "Messaging" "Contacts" "webview Browser2" "webview" "Browser2" "PicoTts" "LatinIME")
+# Loop through the list and remove each package from Android.mk files
+for PACKAGEU in "${OVER_PACKAGES[@]}"; do
+  find vendor/gms -name 'Android.mk' -exec sed -i "/^LOCAL_OVERRIDES_PACKAGES := $PACKAGEU$/d" {} \;
+done
+echo "===== Remove overrides Success ====="
 
 # Export
-export BUILD_USERNAME=derpshit
+export BUILD_USERNAME=devil 
 export BUILD_HOSTNAME=crave
-echo "============="
+echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
-echo "======================================"
-echo "im crave sir you rom sync Done ok bye "
-echo "======================================"
+echo "====== Envsetup Done ======="
 
 # Lunch
-lunch derp_Mi439_4_19-ap2a-userdebug 
+lunch derp_Mi439_4_19-ap3a-userdebug || lunch derp_Mi439_4_19-userdebug
 echo "============="
-
 # Make cleaninstall
 make installclean
 echo "============="
-
 # Build rom
 mka derp
