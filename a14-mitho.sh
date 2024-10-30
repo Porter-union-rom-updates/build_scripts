23524b6f8c6c07b42c6f79b3dcb5bf1c8cb04a00
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-30 22:29:34 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,12 +1,35 @@
-rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#!/bin/bash
 
-#build
+rm -rf .repo/local_manifests/
+
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/Porter-union-rom-updates/local_manifests -b 13/spark .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
 /opt/crave/resync.sh
-. build/envsetup.sh
-riseup mi439 userdebug 
+echo "============="
+echo "Sync success"
+echo "============="
+
+
+# Export
+export BUILD_USERNAME=devil 
+export BUILD_HOSTNAME=crave
+#export MITHORIUM_QCOM_HALS_DEFAULT_VARIANT=LA.UM.9.6.4.r1-05500-89xx.QSSI13.0
+echo "======= Export Done ======"
+
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+lunch derp_Mi439_4_19-userdebug
 make installclean
-rise b
