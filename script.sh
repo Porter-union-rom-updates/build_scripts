eaa68ee0f976d54989f2cd4f7fd6c0a96580b914
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-02 15:57:26 +0600

    Added

--- script.sh
+++ script.sh
@@ -13,55 +13,38 @@ echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
+# Local manifests
+git clone https://github.com/Gtajisan/local_manifests -b a14-crd .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
 # Sync
 /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-# Ty Crave full credit tavukkdoner
-cd packages/apps/Settings
-git remote add tmpRepo https://github.com/tavukkdoner/android_packages_apps_Settings
-git fetch tmpRepo 14.0
-git cherry-pick 75c8e07
-git cherry-pick 7ecc750
-# https://github.com/crdroidandroid/android_packages_apps_Settings/commit/8ae1b733443d0aecacef1f64f2e020c95e42ca22
-git revert 8ae1b73
-git remote remove tmpRepo
-cd ../../../
-# extra 
-
-git fetch https://github.com/Gtajisan/android_packages_apps_Settings
-git cherry-pick 3dd2f59
-git cherry-pick 06ca759
-
-cd lineage-sdk
-git remote add tmpRepo1 https://github.com/tavukkdoner/android_lineage-sdk
-git fetch tmpRepo1 14.0
-git cherry-pick 89ef794
-git remote remove tmpRepo1
-cd ../
-
-cd vendor/lineage
-git remote add tmpRepo2 https://github.com/tavukkdoner/android_vendor_crdroid
-git fetch tmpRepo2 14.0
-git cherry-pick 40fe82e
-git remote remove tmpRepo2
-cd ../../
-
-#if [ ! -e "vendor/lineage-priv" ]; then
-#    curl -O https://raw.githubusercontent.com/tavukkdoner/crDroid-build-signed-script/crdroid/create-signed-env.sh
-#    chmod +x create-signed-env.sh
-#    ./create-signed-env.sh
-#fi
+# keys
+# mkdir vendor/lineage-priv
+# cp build-keys/* vendor/lineage-priv
+# echo "============="
+# echo "Keys copied"
+# echo "============="
 
 # Export
-#export BUILD_USERNAME=Farhan 
-#echo "======= Export Done ======"
+export BUILD_USERNAME=FARHAN 
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
-# Build the ROM
-lunch lineage_Mi439_4_19-ap2a-userdebug && make installclean && mka bacon
 
+# Lunch
+lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap3a-userdebug || lunch lineage_Mi439_4_19-ap2a-userdebug
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
