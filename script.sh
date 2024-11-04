11df1694489517eac41299549e7f2786c9d7029a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-04 17:55:45 +0600

    Added

--- script.sh
+++ script.sh
@@ -21,33 +21,33 @@ git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a15-cr
 /opt/crave/resynctest.sh
 
 # Ty Crave 
-cd packages/apps/Settings
-git remote add tmpRepo https://github.com/tavukkdoner/android_packages_apps_Settings
-git fetch tmpRepo 15.0
-git cherry-pick 860634b
-git cherry-pick 30ec96a
+#cd packages/apps/Settings
+#git remote add tmpRepo https://github.com/tavukkdoner/android_packages_apps_Settings
+#git fetch tmpRepo 15.0
+#git cherry-pick 860634b
+#git cherry-pick 30ec96a
 # https://github.com/crdroidandroid/android_packages_apps_Settings/commit/c6fdcc764c038007993045dfd5cb17999ebf00fb
-git revert c6fdcc7
-git remote remove tmpRepo
-cd ../../../
+#git revert c6fdcc7
+#git remote remove tmpRepo
+#cd ../../../
 
-cd lineage-sdk
-git remote add tmpRepo1 https://github.com/tavukkdoner/android_lineage-sdk
-git fetch tmpRepo1 15.0
-git cherry-pick a3743d1
-git remote remove tmpRepo1
-cd ../
+#cd lineage-sdk
+#git remote add tmpRepo1 https://github.com/tavukkdoner/android_lineage-sdk
+#git fetch tmpRepo1 15.0
+#git cherry-pick a3743d1
+#git remote remove tmpRepo1
+#cd ../
 
-cd vendor/lineage
-git remote add tmpRepo2 https://github.com/tavukkdoner/android_vendor_crdroid
-git fetch tmpRepo2 15.0
-git cherry-pick 2597054
-git remote remove tmpRepo2
+#cd vendor/lineage
+#git remote add tmpRepo2 https://github.com/tavukkdoner/android_vendor_crdroid
+#git fetch tmpRepo2 15.0
+#git cherry-pick 2597054
+#git remote remove tmpRepo2
 #git remote add tmpRepo4 https://github.com/LineageOS/android_vendor_lineage
 #git fetch tmpRepo4 lineage-22.0
 #git cherry-pick d6777fa
 #git remote remove tmpRepo4
-cd ../../
+#cd ../../
 
 #cd build/soong
 #git remote add tmpRepo3 https://github.com/LineageOS/android_build_soong
@@ -62,11 +62,11 @@ export BUILD_HOSTNAME=crave
 
 source build/envsetup.sh
 
-if [ ! -e "vendor/lineage-priv" ]; then
-    curl -O https://raw.githubusercontent.com/tavukkdoner/crDroid-build-signed-script/crdroid/create-signed-env.sh
-    chmod +x create-signed-env.sh
-    ./create-signed-env.sh
-fi
+#if [ ! -e "vendor/lineage-priv" ]; then
+ #   curl -O https://raw.githubusercontent.com/tavukkdoner/crDroid-build-signed-script/crdroid/create-signed-env.sh
+ #   chmod +x create-signed-env.sh
+  #  ./create-signed-env.sh
+#fi
  
 # Build the ROM
 # lunch lineage_Mi439-ap2a-userdebug && make installclean && mka bacon
