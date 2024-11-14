2ffd2d68d9d4cb323cf259e9787e5fdd6743479d
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-14 22:57:49 +0600

    Different

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/AfterLifePrjkt13/android_manifest.git -b 4.0 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b afterlife .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b a13-afterlife .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
