90a2b8316fc48f240ce2ae8cb4524516aacb2cbd
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-18 19:32:54 +0600

    Added

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b infinity-x .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b infinity .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -22,7 +22,7 @@ echo "============="
 
 
 # Export
-export BUILD_USERNAME=FARHAN
+export BUILD_USERNAME=devil
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
