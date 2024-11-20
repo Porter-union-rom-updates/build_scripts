f416f99f1c9df7ab6aacfac9fd168b91460e59d6
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-20 22:46:54 +0600

    It's

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b infinity .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b vic .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -21,10 +21,10 @@ echo "Sync success"
 echo "============="
 
 # remove face unlock 
-rm -rf packages/apps/FaceUnlock
+#rm -rf packages/apps/FaceUnlock
 
 # Export
-export BUILD_USERNAME=FARHAN
+export BUILD_USERNAME=devil
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
