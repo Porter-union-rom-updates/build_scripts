d79bbab4631fd75fdd845f98b7f99e7a8c5a9240
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-20 13:07:59 +0600

    Pha

--- script.sh
+++ script.sh
@@ -3,42 +3,37 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
-echo "=================="
+repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3 --git-lfs 
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b infinity .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b pixelstar/14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# build
+# Sync
 /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-
 # Export
-export BUILD_USERNAME=devil
+export BUILD_USERNAME=FARHAN_UN
 export BUILD_HOSTNAME=crave
 export ALLOW_MISSING_DEPENDENCIES=true
-echo ======= Export Done ======
 echo "======= Export Done ======"
 
 # Set up build environment
-source build/envsetup.sh
+. build/envsetup.sh 
 echo "====== Envsetup Done ======="
 
 # Lunch
-. build/envsetup.sh
-lunch infinity_mi439-ap2a-userdebug
-lunch infinity_mi439-userdebug
+lunch pixelstar_mi439-ap2a-userdebug
+lunch pixelstar_mi439-userdebug
+echo "====== lunch Done ======="
 
-# make install
+# installclean
 make installclean
-
-# make bacon
 mka bacon
