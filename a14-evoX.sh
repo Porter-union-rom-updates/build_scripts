e99513dade3ef5c78c10403751cfb2b41983dde6
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-12-06 08:35:41 +0600

    Extra

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b a15/master .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b MIST/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -36,12 +36,13 @@ echo "======= Export Done ======"
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-# Lunch
-lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap3a-userdebug || lunch lineage_Mi439_4_19-ap2a-userdebug
-make installclean
-mka bacon
-
-# lunch 
+# lunch 1
 . build/envsetup.sh
 brunch Mi439_4_19 userdebug
 brunch Mi439_4_19
+
+# Lunch
+lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap3a-userdebug || lunch lineage_Mi439_4_19-ap2a-userdebug
+mist b
+
+
