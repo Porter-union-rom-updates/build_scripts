ee85f6be5c449291c3038826f52610dbfbfa6141
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-01 22:11:33 +0600

    A15

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b Matrixx-14 .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b Matrixx/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -28,7 +28,7 @@ echo "============="
 # echo "============="
 
 # Export
-export BUILD_USERNAME=devil 
+export BUILD_USERNAME=FARHAN 
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
@@ -47,5 +47,5 @@ echo "============="
 m bacon
 # more 
 . build/envsetup.sh
-brunch Mi439_4_19 userdebug
-brunch Mi439_4_19
+brunch mi439 userdebug
+brunch mi439
