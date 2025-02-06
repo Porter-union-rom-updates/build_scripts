67be52bbbe4f304a985b3af1651ae5e4ac291f86
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-06 08:22:29 +0600

    Evoo

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b Matrixx/15 .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b Evo/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -34,7 +34,7 @@ export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
 # Set up build environment
-source build/envsetup.sh
+. build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
@@ -42,11 +42,10 @@ lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap3a-userdebug ||
 echo "============="
 
 # Make cleaninstall
+lunch lineage_Mi439_4_19-ap4a-userdebug
 make installclean
 echo "============="
+
 # bacon
-m bacon
-# more 
-. build/envsetup.sh
-brunch mi439 userdebug
-brunch mi439
+m evolution
+
