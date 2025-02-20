da174b943ba7f7d2ba778a5b5eeb15e0fea4a214
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-20 08:12:26 +0600

    Added

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Porter-union-rom-updates/local_manifests_rose -b PixelOS/15 .repo/local_manifests
+git clone https://github.com/Porter-union-rom-updates/local_manifests_rose -b Evo/15x .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
