fee53c19d7ff9cd0fd24983d8414a07b5100f59c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-11 04:50:38 +0600

    Added

--- Mist15.sh
+++ Mist15.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b MistOS/15 .repo/local_manifests
+git clone https://github.com/Porter-union-rom-updates/local_manifests_clo -b MistOS/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -19,13 +19,6 @@ echo "============================"
 echo "============="
 echo "Sync success"
 echo "============="
-
-# keys
-# mkdir vendor/lineage-priv
-# cp build-keys/* vendor/lineage-priv
-# echo "============="
-# echo "Keys copied"
-# echo "============="
 
 # Export
 export BUILD_USERNAME=misti 
