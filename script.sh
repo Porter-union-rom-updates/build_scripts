9e6448f1089ed1d0baebe145ceca576ec0132335
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-16 00:44:01 +0600

    More

--- script.sh
+++ script.sh
@@ -6,12 +6,12 @@ echo "Removing existing local_manifests..."
 rm -rf .repo/local_manifests/ || { echo "Failed to remove local_manifests"; exit 1; }
 
 # Initialize matrixx repo
-echo "Initializing PixelOS repo..."
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs || { echo "Repo init failed"; exit 1; }
+echo "Initializing LineageOS_gsi repo..."
+repo init -u https://github.com/MisterZtr/LineageOS-android.git -b lineage-22.1 --git-lfs || { echo "Repo init failed"; exit 1; }
 
 # Clone local_manifests repository
 echo "Cloning local_manifests repository..."
-git clone https://github.com/MisterZtr/treble_manifest.git .repo/local_manifests  -b 14 || { echo "Cloning local_manifests failed"; exit 1; }
+git clone https://github.com/MisterZtr/treble_manifest.git .repo/local_manifests  -b 15-los || { echo "Cloning local_manifests failed"; exit 1; }
 
 # Sync the repositories
 echo "Syncing the repositories..."
@@ -20,10 +20,10 @@ echo "Syncing the repositories..."
 
 
 # Set build details
-export BUILD_USERNAME=FARHAN-MUH-TASIM
+export BUILD_USERNAME=gsi_hunt
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export MATRIXX_MAINTAINER=FARHAB!gsi
+
 
 
 # Set up build environment
@@ -35,7 +35,7 @@ echo "Configuring lunch..."
 lunch treble_arm64_bN-ap2a-userdebug || { echo "Lunch configuration failed"; exit 1; }
 
 # Build the matrixx
-echo "Building matrixx..."
+echo "LineageOS_gsi..."
 make systemimage || { echo "Build failed"; exit 1; }
 
-echo "matrixx Build completed successfully!"
+echo "LineageOS_gsi Build completed successfully!"
