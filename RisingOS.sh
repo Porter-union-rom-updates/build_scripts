d593967e2326a99e5db3d34d2085910c9c9e9b57
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-27 10:48:29 +0600

    Dr

--- RisingOS.sh
+++ RisingOS.sh
@@ -36,6 +36,9 @@ echo "======= Export Done ======"
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
+# Lunch
+lunch lineage_mi439-userdebug || lunch lineage_mi439-ap3a-userdebug || lunch lineage_mi439-ap2a-userdebug
+echo "============="
 
 # Lunch
 riseup mi439 userdebug 
