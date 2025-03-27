9f3d3827803e5ced79f015efcbb3797988be0779
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-27 11:36:48 +0600

    Pi

--- RisingOS.sh
+++ RisingOS.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b rise_b .repo/local_manifests
+git clone https://github.com/Porter-union-rom-updates/local_manifests_clo -b rise_b .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -28,7 +28,7 @@ echo "============="
 # echo "============="
 
 # Export
-export BUILD_USERNAME=FARHAN 
+export BUILD_USERNAME=dev_evil
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
